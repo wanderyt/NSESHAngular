36f5ed6d7211491c6ba4dee8c76ad22562e53293
wanderyt <wanderyt@hotmail.com>
2014-07-18 21:55:32 +0800

    Link main page and file server.

---
+++ file-server.js
@@ -0,0 +1,165 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: I077479
+ * Date: 7/17/14
+ * Time: 1:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var util = require('util'),
+    http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    events = require('events'),
+    path=require("path"),
+    mime=require("./mime").mime;
+
+var ROOT = "C:\\";
+
+if(!path.existsSync(ROOT)){
+    util.error(ROOT+"文件夹不存在，请重新制定根文件夹！");
+    process.exit();
+}
+
+//显示文件夹下面的文件
+function listDirectory(parentDirectory,req,res){
+    fs.readdir(parentDirectory,function(error,files){
+        var body=formatBody(parentDirectory,files);
+        res.writeHead(200,{
+            "Content-Type":"text/html;charset=utf-8",
+            "Content-Length":Buffer.byteLength(body,'utf8'),
+            "Server":"NodeJs("+process.version+")"
+        });
+        res.write(body,'utf8');
+        res.end();
+    });
+}
+
+//显示文件内容
+function showFile(file,req,res){
+    fs.readFile(filename,'binary',function(err,file){
+        var contentType=mime.lookupExtension(path.extname(filename));
+        res.writeHead(200,{
+            "Content-Type":contentType,
+            "Content-Length":Buffer.byteLength(file,'binary'),
+            "Server":"NodeJs("+process.version+")"
+        });
+        res.write(file,"binary");
+        res.end();
+    })
+}
+
+//在Web页面上显示文件列表，格式为<ul><li></li><li></li></ul>
+function formatBody(parent,files){
+    var res=[],
+        length=files.length;
+    res.push("<!doctype>");
+    res.push("<html>");
+    res.push("<head>");
+    res.push("<meta http-equiv='Content-Type' content='text/html;charset=utf-8'></meta>");
+    res.push("<title>Node.js文件服务器</title>");
+    res.push("<style>li:nth-of-type(even){background-color: #EEEEEE;}</style>");
+    res.push("</head>");
+    res.push("<body width='100%'>");
+    res.push("<div style='position:relative;bottom:5px;height:30px;background:gray'>");
+    res.push("<div style='margin:0 auto;height:100%'>");
+    res.push("<span style='font-size: 22px;line-height: 30px;text-align: left;font-weight: bold;float:left;color: #ffffff;'>Directory Listing For "
+        + parent
+        + "</span>");
+    res.push("<span style='font-size: 22px;line-height: 30px;text-align: right;font-weight: bold;float:right;'>"
+        + "<a style='color:#ffffff' href='http://localhost:8000/app/index.html'>Return to NSESH page</a>"
+        + "</span>");
+    res.push("</div>");
+    res.push("<hr>");
+    res.push("<ul style='list-style: none;padding-left:0px'>");
+    if(parent != ROOT) {
+        var parentURL = parent.split(ROOT)[1];
+        var basename = path.basename(parent);
+        var parentDir = parentURL.split(basename)[0];
+        console.log("parentURL : " + parentURL);
+        console.log("basename : " + basename);
+        console.log("parentDir : " + parentDir);
+        res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'>" +
+            "<a href='"+"/"+parentDir+"'>../</a></li>");
+    }
+    files.forEach(function(val,index){
+        console.log("val : " + val);
+        console.log("parent : " + parent);
+        try {
+            var stat=fs.statSync(path.join(parent,val));
+            var parentSplits = parent.split(ROOT);
+            var parentPath = parentSplits[1];
+            if(stat.isDirectory(val)) {
+                val = path.basename(val) + "/";
+            } else {
+                val=path.basename(val);
+            }
+            if(parentPath == "") {
+                res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'><a href='"+"/"+val+"'>"+val+"</a></li>");
+            } else {
+                res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'><a href='"+"/"+parentPath+"/"+val+"'>"+val+"</a></li>");
+            }
+        } catch(e) {
+            //res.push("<li>" + e.message + "</li>");
+        }
+    });
+    res.push("</ul>");
+    res.push("<hr>");
+    res.push("<div style='position:relative;bottom:5px;height:25px;background:gray'>");
+    res.push("<div style='margin:0 auto;height:100%;line-height:25px;text-align:center'>Powered By Node.js</div>");
+    res.push("</div>")
+    res.push("</body>");
+    return res.join("");
+}
+
+//如果文件找不到，显示404错误
+function write404(req,res){
+    var body="文件不存在:-(";
+    res.writeHead(404,{
+        "Content-Type":"text/html;charset=utf-8",
+        "Content-Length":Buffer.byteLength(body,'utf8'),
+        "Server":"NodeJs("+process.version+")"
+    });
+    res.write(body);
+    res.end();
+}
+
+// 处理文件服务器url
+function handleFileServerURL(req, res){
+    //将url地址的中的%20替换为空格，不然Node.js找不到文件
+    var pathname=url.parse(req.url).pathname.replace(/%20/g,' '),
+        re=/(%[0-9A-Fa-f]{2}){3}/g;
+    //能够正确显示中文，将三字节的字符转换为utf-8编码
+    pathname=pathname.replace(re,function(word){
+        var buffer=new Buffer(3),
+            array=word.split('%');
+        array.splice(0,1);
+        array.forEach(function(val,index){
+            buffer[index]=parseInt('0x'+val,16);
+        });
+        return buffer.toString('utf8');
+    });
+    console.log("pathname : " + pathname);
+    if(pathname=='/'){
+        listDirectory(ROOT,req,res);
+    }else{
+        filename=path.join(ROOT,pathname);
+        path.exists(filename,function(exists){
+            if(!exists){
+                util.error('找不到文件'+filename);
+                write404(req,res);
+            }else{
+                fs.stat(filename,function(err,stat){
+                    if(stat.isFile()){
+                        showFile(filename,req,res);
+                    }else if(stat.isDirectory()){
+                        listDirectory(filename,req,res);
+                    }
+                });
+            }
+        });
+    }
+}
+
+module.exports = {
+    handleFileServerURL : handleFileServerURL
+};
--- web-server.js
+++ web-server.js
@@ -6,8 +6,8 @@ var util = require('util'),
     url = require('url'),
     events = require('events'),
     mysql = require('./mysql-connection'),
-    path=require("path"),
-    mime=require("./mime").mime;
+    path = require("path"),
+    fileServer = require('./file-server');
 
 var DEFAULT_PORT = 8000;
 var FILE_PORT    = 8003;
@@ -18,42 +18,8 @@ function main(argv) {
         'HEAD': createServlet(StaticServlet)
     }).start(Number(argv[2]) || DEFAULT_PORT);
 
-    //创建服务器
-    http.createServer(function(req,res){
-        //将url地址的中的%20替换为空格，不然Node.js找不到文件
-        var pathname=url.parse(req.url).pathname.replace(/%20/g,' '),
-            re=/(%[0-9A-Fa-f]{2}){3}/g;
-        //能够正确显示中文，将三字节的字符转换为utf-8编码
-        pathname=pathname.replace(re,function(word){
-            var buffer=new Buffer(3),
-                array=word.split('%');
-            array.splice(0,1);
-            array.forEach(function(val,index){
-                buffer[index]=parseInt('0x'+val,16);
-            });
-            return buffer.toString('utf8');
-        });
-        console.log("pathname : " + pathname);
-        if(pathname=='/'){
-            listDirectory(root,req,res);
-        }else{
-            filename=path.join(root,pathname);
-            path.exists(filename,function(exists){
-                if(!exists){
-                    util.error('找不到文件'+filename);
-                    write404(req,res);
-                }else{
-                    fs.stat(filename,function(err,stat){
-                        if(stat.isFile()){
-                            showFile(filename,req,res);
-                        }else if(stat.isDirectory()){
-                            listDirectory(filename,req,res);
-                        }
-                    });
-                }
-            });
-        }
-    }).listen(FILE_PORT);
+    http.createServer(fileServer.handleFileServerURL).listen(FILE_PORT);
+    util.puts('File Server running at http://localhost:' + FILE_PORT + '/');
 }
 
 function escapeHtml(value) {
@@ -319,112 +285,6 @@ function dispatchURLByDatabase(path) {
         return mysql.getOneActivityPhotoByType;
     }
 }
-
-// -------------------- File System ----------------------------
-//www根目录
-var root="C:\\";
-
-if(!path.existsSync(root)){
-    util.error(root+"文件夹不存在，请重新制定根文件夹！");
-    process.exit();
-}
-
-//显示文件夹下面的文件
-function listDirectory(parentDirectory,req,res){
-    fs.readdir(parentDirectory,function(error,files){
-        var body=formatBody(parentDirectory,files);
-        res.writeHead(200,{
-            "Content-Type":"text/html;charset=utf-8",
-            "Content-Length":Buffer.byteLength(body,'utf8'),
-            "Server":"NodeJs("+process.version+")"
-        });
-        res.write(body,'utf8');
-        res.end();
-    });
-
-}
-
-//显示文件内容
-function showFile(file,req,res){
-    fs.readFile(filename,'binary',function(err,file){
-        var contentType=mime.lookupExtension(path.extname(filename));
-        res.writeHead(200,{
-            "Content-Type":contentType,
-            "Content-Length":Buffer.byteLength(file,'binary'),
-            "Server":"NodeJs("+process.version+")"
-        });
-        res.write(file,"binary");
-        res.end();
-    })
-}
-
-//在Web页面上显示文件列表，格式为<ul><li></li><li></li></ul>
-function formatBody(parent,files){
-    var res=[],
-        length=files.length;
-    res.push("<!doctype>");
-    res.push("<html>");
-    res.push("<head>");
-    res.push("<meta http-equiv='Content-Type' content='text/html;charset=utf-8'></meta>")
-    res.push("<title>Node.js文件服务器</title>");
-    res.push("<style>li:nth-of-type(even){background-color: #EEEEEE;}</style>");
-    res.push("</head>");
-    res.push("<body width='100%'>");
-    res.push("<div style='position:relative;bottom:5px;height:25px;background:gray'>");
-    res.push("<div style='margin:0 auto;font-size: 22px;height:100%;line-height:25px;text-align:left;font-weight:bold;color:#ffffff'>Directory Listing For "
-        + parent
-        + "</div>");
-    res.push("<hr>");
-    res.push("<ul style='list-style: none;padding-left:0px'>");
-    if(parent != root) {
-        //var parentURL = path.split(parent)[0];
-        console.log("path : " + path);
-        console.log("parentURL : " + path.dirname());
-        //res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'><a href='"+"/"+parentURL+"'>..</a></li>");
-    }
-    files.forEach(function(val,index){
-        console.log("val : " + val);
-        console.log("parent : " + parent);
-        try {
-            var stat=fs.statSync(path.join(parent,val));
-            var parentSplits = parent.split(root);
-            var parentPath = parentSplits[1];
-            if(stat.isDirectory(val)) {
-                val = path.basename(val) + "/";
-            } else {
-                val=path.basename(val);
-            }
-            if(parentPath == "") {
-                res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'><a href='"+"/"+val+"'>"+val+"</a></li>");
-            } else {
-                res.push("<li style='font-family: monospace;font-size: 16px;line-height: 16px;margin: 6px 0;text-indent: 20px;'><a href='"+"/"+parentPath+"/"+val+"'>"+val+"</a></li>");
-            }
-        } catch(e) {
-            //res.push("<li>" + e.message + "</li>");
-        }
-    });
-    res.push("</ul>");
-    res.push("<hr>");
-    res.push("<div style='position:relative;bottom:5px;height:25px;background:gray'>");
-    res.push("<div style='margin:0 auto;height:100%;line-height:25px;text-align:center'>Powered By Node.js</div>");
-    res.push("</div>")
-    res.push("</body>");
-    return res.join("");
-}
-
-//如果文件找不到，显示404错误
-function write404(req,res){
-    var body="文件不存在:-(";
-    res.writeHead(404,{
-        "Content-Type":"text/html;charset=utf-8",
-        "Content-Length":Buffer.byteLength(body,'utf8'),
-        "Server":"NodeJs("+process.version+")"
-    });
-    res.write(body);
-    res.end();
-}
-
-// ---------------------------- End --------------------------------
 
 // Must be last,
 main(process.argv);
